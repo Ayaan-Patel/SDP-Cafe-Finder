3d5526ce7d3da58824643f51d244aa28e7e4319c
Ghaith1345 <90782275+Ghaith1345@users.noreply.github.com>
2021-11-23 10:04:59 -0500

    Create SearchBar.py

---
+++ Ghaith/SearchBar.py
@@ -0,0 +1,13 @@
+<div class="wrapper">
+    <ul>
+<li>Your Location
+              <ul>
+<li>Mumbai</li>
+<li>Pune</li>
+<li>KolKata</li>
+<li>Hyderabad</li>
+<li>Bangalor</li>
+<li>Delhi</li>
+<li>Chennai</li>
+<li>Ahmedabad</li>
+</div>
